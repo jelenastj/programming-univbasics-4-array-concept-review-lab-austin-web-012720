cc52881092c292cdcc4f9d669c0736f534cef1c1
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-10-31 17:21:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,8 @@ def find_element_index(array, value_to_find)
   
   array.include? (value_to_find)
      puts array.index (value_to_find)
-  
 end
-
+d=find_element_index
 
 
 def find_max_value(array)
